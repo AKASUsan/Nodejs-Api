13b7aa01f94972442f7106bd847c5c797f655812
AKASUsan <kasubaranime@gmail.com>
2025-12-13 20:05:17 +0700

    Editing API

--- asset/As.js
+++ asset/As.js
@@ -1,25 +1,38 @@
 const http = require("http");
 const fs = require("fs");
-// module URL
+//module URL
 const { URL } = require("url");
+const path = require("path");
 
-const path = fs.readFileSync(`${__dirname}/../templates/index.html`, "utf-8");
 const server = http.createServer((req, res) => {
   const myurl = new URL(req.url, `http://${req.headers.host}`, true);
-  const product = myurl.pathname;
-  if (product === "/" || product === "/home") {
-    res.end(path);
-  } else if (product === "/product?=id?") {
-    //TODO:
-    //product show button and click imags
-    //select all file
-    //open file
-  } else {
-    res.writeHead(404);
-    res.end(`<h1>Error</h1>`);
+
+  try {
+    const templatesPath = "../templates";
+    const files = fs.readdirSync(templatesPath);
+    console.log("Files in the directory:", files);
+
+    files.forEach((file) => {
+      const fullPath = path.join(templatesPath, file);
+      console.log("Full path:", fullPath);
+      const product = [fullPath];
+    });
+  } catch (err) {
+    console.log("Error reading directory:", err);
   }
 
-  console.log(`URL: ${product}`);
+  // if (product === "/" || product === "/home") {
+  //   res.end();
+  // } else if (product === "/product") {
+  //   //TODO:
+  //   //product show link and click imags
+  //   //สรัางarray
+  // } else {
+  //   res.writeHead(404);
+  //   res.end(`<h1>Error</h1>`);
+  // }
+
+  // console.log(`URL: ${product}`);
 });
 
 server.listen(8080, () => {
